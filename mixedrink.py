5574f6460c21dfd347faddc9fed92d02f8190098
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-06-30 14:03:32 +0100

    Create mixedrink.py

---
+++ mixedrink.py
@@ -0,0 +1,7 @@
+import random
+alcohol = ['Gin', 'Rum', 'Vodka', 'Whisky']
+mixer = ['Pepsi', 'Water', 'Coca-Cola', 'Orange', 'mango']
+ 
+print("Mix your drink with : ")
+print(alcohol[random.randint(0,3)])
+print(mixer[random.randint(0,3)])
